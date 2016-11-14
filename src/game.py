436c1a90fcf269e319303d5d831957ac061aa44c
Tommy Ip <hkmp7tommy@gmail.com>
2016-11-14 20:56:38 +0000

    Added basic movement to the snake

---
+++ src/game.py
@@ -0,0 +1,98 @@
+import pyglet
+from pyglet.window import key
+
+pyglet.resource.path = ['../resources']
+pyglet.resource.reindex()
+
+window = pyglet.window.Window(800, 600)
+pyglet.gl.glClearColor(0.129, 0.129, 0.129, 1)  # Primary text color
+
+object_batch = pyglet.graphics.Batch()
+
+body_image = pyglet.resource.image('body.png')
+head_image = pyglet.resource.image('head.png')
+body_image.width, body_image.height = 20, 20
+head_image.width, head_image.height = 20, 20
+
+images = [body_image, head_image]
+
+for image in images:
+    image.anchor_x = image.width / 2
+    image.anchor_y = image.width / 2
+
+
+class SnakeHead(pyglet.sprite.Sprite):
+    MOVE_SPEED = 120
+
+    def __init__(self, *args, **kwargs):
+        super(SnakeHead, self).__init__(*args, **kwargs)
+        self.direction = 'RIGHT'
+        self.past_location = []
+
+    def update(self, dt):
+        if self.direction == 'FORWARD':
+            self.y += int(SnakeHead.MOVE_SPEED * dt)
+        elif self.direction == 'BACKWARD':
+            self.y -= int(SnakeHead.MOVE_SPEED * dt)
+        elif self.direction == 'RIGHT':
+            self.x += int(SnakeHead.MOVE_SPEED * dt)
+        elif self.direction == 'LEFT':
+            self.x -= int(SnakeHead.MOVE_SPEED * dt)
+
+        self.past_location.append((self.x, self.y))
+
+
+class SnakeBody(pyglet.sprite.Sprite):
+    id = 0
+
+    def __init__(self, *args, **kwargs):
+        super(SnakeBody, self).__init__(*args, **kwargs)
+        self.id = SnakeBody.id + 1
+
+    def update(self, dt, pos_x, pos_y):
+        self.x, self.y = pos_x, pos_y
+
+
+lb_score = pyglet.text.Label(text="Score: 0", x=10, y=10, batch=object_batch)
+snake_head = SnakeHead(img=head_image, x=800//2, y=600//2, batch=object_batch)
+
+snake_bodies = []
+
+
+@window.event
+def on_draw():
+    window.clear()
+    object_batch.draw()
+
+
+@window.event
+def on_key_press(symbol, modifier):
+    if symbol == key.UP:
+        snake_head.direction = 'FORWARD'
+    elif symbol == key.DOWN:
+        snake_head.direction = 'BACKWARD'
+    elif symbol == key.RIGHT:
+        snake_head.direction = 'RIGHT'
+    elif symbol == key.LEFT:
+        snake_head.direction = 'LEFT'
+    elif symbol == key.SPACE:
+        snake_bodies.append(
+            SnakeBody(img=body_image,
+                      x=snake_head.past_location[-SnakeBody.id-2][0],
+                      y=snake_head.past_location[-SnakeBody.id-2][1],
+                      batch=object_batch
+                      )
+        )
+
+
+def update(dt):
+    snake_head.update(dt)
+    for index, body in enumerate(snake_bodies):
+        body.update(dt,
+                    snake_head.past_location[-index-2][0],
+                    snake_head.past_location[-index-2][1])
+
+
+if __name__ == "__main__":
+    pyglet.clock.schedule_interval(update, 1/6)
+    pyglet.app.run()
